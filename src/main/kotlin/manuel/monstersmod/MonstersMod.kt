754b7f740e3f240484319fc5fd259ca88b8b8ae1
Manuel Ramos <manumanram2@gmail.com>
2026-05-29 19:58:02 +0200

    Empiezo a comentar cosillas para no olvidarme de lo que hacen las funciones y tal

--- src/main/kotlin/manuel/monstersmod/MonstersMod.kt
+++ src/main/kotlin/manuel/monstersmod/MonstersMod.kt
@@ -1,7 +1,11 @@
 package manuel.monstersmod
 
 import manuel.monstersmod.items.MisItems
+import manuel.monstersmod.items.MisItems.Companion.registrarItem
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
 import org.slf4j.LoggerFactory
 
 object MonstersMod : ModInitializer {
@@ -9,6 +13,8 @@ object MonstersMod : ModInitializer {
 	const val MOD_ID: String = "monstersmod"
 
 	override fun onInitialize() {
-		MisItems.registrarItem()
+
+
+		MisItems.agregarAlInventario(MisItems.MANGO_LOCO, ItemGroups.INGREDIENTS)
 	}
 }
--- src/main/kotlin/manuel/monstersmod/items/MisItems.kt
+++ src/main/kotlin/manuel/monstersmod/items/MisItems.kt
@@ -2,10 +2,15 @@ package manuel.monstersmod.items
 
 import manuel.monstersmod.MonstersMod
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registry
 import net.minecraft.registry.Registries
 import net.minecraft.util.Identifier
+import net.minecraft.item.ItemGroup
+import net.minecraft.registry.RegistryKey
 
 class MisItems {
 
@@ -13,12 +18,39 @@ class MisItems {
 
         val MANGO_LOCO: Item? = registrarItem("mangoloco", Item(FabricItemSettings()))
 
+
+        // Funciones //
+
+        /*
+
+        Registry.register() crea el registro del item en la tabla de registros de los ITEMS (Registries.ITEM).
+        Con Identifier() le pasas el id del registro y le pasas luego el item, que es una instancia de la clase Item
+        y le pasas al constructor una instancia de la clase Item.Settings o de la clase FabricItemSettings (lo mismo
+        pero con mas funcionalidades). Éste será el objeto de configuración del item.
+
+        */
+
         fun registrarItem(idItem: String, item: Item ): Item? {
             return Registry.register(
                 Registries.ITEM,
                 Identifier(MonstersMod.MOD_ID, idItem),
                 item
                 )
+        }
+
+        /*
+
+        Obtenemos el evento del grupo del inventario creativo, por ejemplo pasamos ItemGroups.INGREDIENTS,
+        que es el identificador de la pestaña de ingredientes del inventario creativo. El identificador de la
+        pestaña se lo pasamos a la función ItemGroupEvents.modifyEntriesEvent() para obtener el evento y con la
+        función register() al evento nos enganchamos/suscribimos al evento y le pasamos el item que queremos agregar
+        mediante una lambda. {entries -> entries.add(item)}, entries es el objeto que se va a inyectar en el evento
+        cuando se dispare y con add() añadimos el item.
+
+         */
+
+        fun agregarAlInventario(item: Item?, tabCreativo: RegistryKey<ItemGroup>) {
+            ItemGroupEvents.modifyEntriesEvent(tabCreativo).register { entries -> entries.add(item) }
         }
     }
 
